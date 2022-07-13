8f1b8615423f3a51cde2d10f9dbc18719362326c
cjones1047 <cjmj1047@gmail.com>
2022-07-13 08:46:48 -0500

    Updated ERD

---
+++ project/controller/user_routes.js
@@ -0,0 +1,97 @@
+/////////////////////////////////////////
+// First, import dependencies
+/////////////////////////////////////////
+const express = require('express')
+const User = require('../models/user.js')
+// bcrypt is used to hash(read: encrypt) passwords
+const bcrypt = require('bcryptjs')
+
+/////////////////////////////////////////
+// Create a router
+/////////////////////////////////////////
+const router = express.Router()
+
+/////////////////////////////////////////
+// list out our routes
+/////////////////////////////////////////
+// two sign up routes
+// one GET to show the form
+router.get('/signup', (req, res) => {
+    res.render('users/signup.liquid')
+})
+// one POST to make the db request
+router.post('/signup', async (req, res) => {
+    console.log('this is our initial request body:', req.body)
+    req.body.password = await bcrypt.hash(
+        req.body.password,
+        await bcrypt.genSalt(10)
+    )
+
+    // now that our password is hashed, we can create a user
+    console.log('this is our request body AFTER hashing:', req.body)
+    User.create(req.body)
+        .then(user => {
+            console.log('this is the new user', user)
+            res.redirect('/users/login')
+        })
+        .catch(error => {
+            console.log(error)
+            res.json(error)
+        })
+})
+
+// two login routes
+// one GET to show the form
+router.get('/login', (req, res) => {
+    res.render('users/login.liquid')
+})
+// one POST to login and create the session
+router.post('/login', async (req, res) => {
+    console.log('this is the request object:', req)
+    // destructure dat from request body
+    const { username, password } = req.body
+    console.log('this is the session', req.session)
+    User.findOne({username})
+        .then(async (user) => {
+            // we check if the user exists
+            // if they do, we'll compare passwords and make sure it's correct
+            if (user) {
+                // compare the password
+                // bcrypt.compare evaluates to a truthy or falsy value
+                const result = await bcrypt.compare(password, user.password)
+
+                if (result) {
+                    req.session.username = username
+                    req.session.loggedIn = true
+                    req.session.userId = user._id
+                    console.log('this is the session after login', req.session)
+                    res.redirect('/fruits')
+                } else {
+                    res.json({error: 'username or password is incorrect'})
+                }
+            } else {
+                res.json({error: 'user does not exist'})
+            }
+        }) 
+        .catch(error => {
+            console.log(error)
+            res.json(error)
+        })    
+})
+
+// logout route
+// can be a GET that calls destroy on our session
+router.get('/logout', (req, res) => {
+    // destroy the session and redirect to the main page
+    req.session.destroy(ret => {
+        console.log('this is the error in logout', ret)
+        console.log('session has been destroyed')
+        console.log(req.session)
+        res.redirect('/fruits')
+    })
+})
+
+/////////////////////////////////////////
+// export our router
+/////////////////////////////////////////
+module.exports = router
---
+++ project/server.js
@@ -0,0 +1,74 @@
+//==============================
+// import dependencies
+//==============================
+
+// this allows us to load our env variables
+require('dotenv').config()
+
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+
+// bring in our session middleware
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+const navRoutes = require('.controller/nav_routes.js')
+const stockRoutes = require('./controller/stock_routes.js')
+const portfolioRoutes = require('./controller/portfolio_routes.js')
+const userRoutes = require('./controller/user_routes.js')
+
+//==============================
+// Create our express application object
+//==============================
+const app = require('liquid-express-views')(express())
+
+//==============================
+// General Middleware
+//==============================
+// this is for request logging
+app.use(morgan('tiny'))
+app.use(methodOverride('_method'))
+// parses urlencoded request bodies
+app.use(express.urlencoded({ extended: false }))
+// to serve files from public statically
+app.use(express.static('public'))
+
+// here's the middleware that sets up our sessions
+app.use(
+	session({
+		secret: process.env.SECRET,
+		store: MongoStore.create({
+			mongoUrl: process.env.DATABASE_URI
+		}),
+		saveUninitialized: true,
+		resave: false
+	})
+)
+
+//==============================
+// Routes Middleware
+//==============================
+// middleware so all of navRoutes will have '/title' prepended
+app.use('/title', navRoutes)
+
+// middleware so all of stockRoutes will have '/title/stocks' prepended
+app.use('/title/stocks', stockRoutes)
+
+// middleware so all of portfolioRoutes will have '/title/portfolios' prepended
+app.use('/title/portfolios', portfolioRoutes)
+
+// middleware so all of userRoutes will have '/users' prepended
+app.use('/users', userRoutes)
+
+app.get('/', (req, res) => {
+	res.redirect('/title')
+})
+
+//==============================
+// Server Listener
+//==============================
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`app is listening on port: ${PORT}`)
+})
