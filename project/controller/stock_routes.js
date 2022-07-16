87ae7a420703cd0f5832f3b906a7e3b22c07071e
cjones1047 <cjmj1047@gmail.com>
2022-07-16 18:23:10 -0500

    Stock-show.liquid renders metrics inside of card

--- project/controller/stock_routes.js
+++ project/controller/stock_routes.js
@@ -17,18 +17,18 @@ router.put('/searchedStock', (req, res) => {
     const quickfsKey = process.env.QUICKFS_API_KEY
 
     async function scrapeCurrentPrice(priceUrl) {
-        // const browser = await puppeteer.launch();
-        // const page = await browser.newPage();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-        // await page.goto(priceUrl);
+        await page.goto(priceUrl);
 
-        // //identify element with attribute selector
-        // const el1 = await page.$("fin-streamer[class='Fw(b) Fz(36px) Mb(-4px) D(ib)']")
-        // //obtain text
-        // const el1Text = await (await el1.getProperty('textContent')).jsonValue()
-        // console.log("Obtained text is: " + el1Text)
+        //identify element with attribute selector
+        const el1 = await page.$("fin-streamer[class='Fw(b) Fz(36px) Mb(-4px) D(ib)']")
+        //obtain text
+        const el1Text = await (await el1.getProperty('textContent')).jsonValue()
+        console.log("Obtained text is: " + el1Text)
 
-        // const lastSharePrice = el1Text
+        const lastSharePrice = el1Text
 
         // // //identify element with attribute selector
         // // const el2 = await page.$("table[class='W(100%) M(0)']")
@@ -38,7 +38,7 @@ router.put('/searchedStock', (req, res) => {
         
         // // const priceHx = el2Text (USE priceHx AS TEMPLATE LITERAL FARTHER DOWN)
 
-        // browser.close()
+        browser.close()
 
         fetch(`https://public-api.quickfs.net/v1/data/all-data/${searchedStock}:US?api_key=${quickfsKey}`)
             .then(response => response.json())
@@ -478,7 +478,11 @@ router.put('/searchedStock', (req, res) => {
 
                 // console.log(tableData)
 
-                res.render('pages/show-stock.liquid', { tableData, metaData, ttmData })
+                const cardMetrics = {
+                    priceToRevenue: (lastSharePrice/tableData.row2[12]).toFixed(2)
+                }
+
+                res.render('pages/show-stock.liquid', { lastSharePrice, cardMetrics, tableData, metaData, ttmData })
             })
             .catch(err => console.error(err));
 
