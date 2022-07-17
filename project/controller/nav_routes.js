33497345608923e626ea518722b49afb5a7e989f
cjones1047 <cjmj1047@gmail.com>
2022-07-17 13:04:22 -0500

    All routes from my-stocks page working

--- project/controller/nav_routes.js
+++ project/controller/nav_routes.js
@@ -1,12 +1,27 @@
 const express = require('express')
 // making a router
 const router = express.Router()
+// importing Stock model to access database
+const Stock = require('../models/stock.js')
 // this allows us to load our env variables
 require('dotenv').config()
 
 ///////////////////////////////////////////////////
 // Routes
 ///////////////////////////////////////////////////
+
+// DELETE
+router.delete('/my-stocks/:id', (req, res) => {
+    const stockId = req.params.id
+
+    Stock.findByIdAndRemove(stockId)
+        .then(stock => {
+            res.redirect('/title/my-stocks')
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
 
 // GET - Home Page
 // localhost:3000/title
@@ -18,6 +33,58 @@ router.get('/', (req, res) => {
 // localhost:3000/title/search-stock
 router.get('/search-stock', (req, res) => {
     res.render('pages/search.liquid')
+})
+
+// GET - Index/My Stocks Page
+// localhost:3000/title/my-stocks
+router.get('/my-stocks', (req, res) => {
+    // mongoose to find all stocks
+    Stock.find({})
+        .then(stocks => {
+            // res.json(stocks) - return stocks as json
+            res.render('pages/index-stocks.liquid', { stocks })
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+// POST - create a Stock recently added
+router.post('/my-stocks', (req, res) => {
+    
+    // when we have user-specific stocks, we'll add a username upon creation
+    // remember, when we logged in, we saved the username to the session object
+    // TODO: need to get a users ._id somehow and change this line
+    // req.body.owner = req.session.userId
+
+    Stock.create(req.body)
+        .then(stock => {
+            console.log(stock)
+            res.redirect('/title/my-stocks')
+            // res.json(stock)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+// POST - create a Stock recently added
+router.delete('/my-stocks', (req, res) => {
+    
+    // when we have user-specific stocks, we'll add a username upon creation
+    // remember, when we logged in, we saved the username to the session object
+    // TODO: need to get a users ._id somehow and change this line
+    // req.body.owner = req.session.userId
+
+    Stock.create(req.body)
+        .then(stock => {
+            console.log(stock)
+            res.redirect('/title/my-stocks')
+            // res.json(stock)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 })
 
 ///////////////////////////////////////////////////
---
+++ project/models/stock.js
@@ -0,0 +1,21 @@
+// using an already connected mongoose NOT a fresh one from node_modules
+const mongoose = require('./connection.js')
+
+// inside of mongoose, I want the keys named 'Schema' and 'model' to be used without specifying 'mongoose', so I'm using this destructuring syntax
+const { Schema, model } = mongoose
+
+const stockSchema = new Schema({
+    name: String,
+    symbol: String,
+    lastPriceViewed: Number,
+    ourBuyPrice: Number,
+    subindustry: String
+}, {
+    timestamp: true
+})
+
+// need to make a model
+// this COLLECTION will be called 'stocks' (lowercase, plural of 'Fruit')
+const Stock = model('Stock', stockSchema)
+
+module.exports = Stock
